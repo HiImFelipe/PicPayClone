c5b033036a994dfa7d32417680794bbd04c5eb08
Felipe Santarosa <snowychest@gmail.com>
2021-05-31 06:37:40 -0300

    feat: Create suggestions section in home page

---
+++ src/components/Suggestions/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import {Container, Option, Image, Label} from './styles';
+import img1 from '../../images/01.png';
+import img2 from '../../images/02.png';
+import img3 from '../../images/03.png';
+import img4 from '../../images/04.png';
+import img5 from '../../images/05.png';
+import img6 from '../../images/06.png';
+import img7 from '../../images/07.png';
+
+const items = [
+  {
+    image: img1,
+    label: 'Doações',
+  },
+  {
+    image: img2,
+    label: 'Doações',
+  },
+  {
+    image: img3,
+    label: 'Doações',
+  },
+  {
+    image: img4,
+    label: 'Doações',
+  },
+  {
+    image: img5,
+    label: 'Doações',
+  },
+  {
+    image: img6,
+    label: 'Doações',
+  },
+  {
+    image: img7,
+    label: 'Doações',
+  },
+];
+
+const Suggestions = () => {
+  return (
+    <Container>
+      {items.map((item, index) => (
+        <Option key={index}>
+          <Image source={item.image} />
+          <Label>{item.label}</Label>
+        </Option>
+      ))}
+    </Container>
+  );
+};
+
+export default Suggestions;
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Platform} from 'react-native';
+import {MaterialCommunityIcons, AntDesign} from '@expo/vector-icons';
+
+import {Wrapper, Container, Header, BalanceContainer, BalanceTitle, Balance} from './styles';
+import Suggestions from '../../components/Suggestions';
 
 const Home = () => {
-  return <Text>Home</Text>;
+  return (
+    <Wrapper OperatingSystem={Platform.OS}>
+      <Container>
+        <Header>
+          <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c86e" />
+
+          <BalanceContainer>
+            <BalanceTitle>Meu Saldo</BalanceTitle>
+            <Balance>R$22,90</Balance>
+          </BalanceContainer>
+
+          <AntDesign name="gift" size={30} color="#10c86e" />
+        </Header>
+
+        <Suggestions />
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Home;
